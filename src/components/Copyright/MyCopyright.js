75cbd70738d8dd918c17b681486f82394cefb8e1
MUKUL <mukul.2226csit1115@kiet.edu>
2024-12-10 12:05:56 +0530

    Hotel Managment System

---
+++ src/components/Copyright/MyCopyright.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MyCopyright.css'
+const MyCopyright = () => {
+    return (
+        <div className='copyrightBar'>
+            <p>The Tiwari Hotels Copyright</p>
+        </div>
+    )
+}
+
+export default MyCopyright
